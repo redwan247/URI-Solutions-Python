a7325688fe573652930e62999f5c23da11e69dae
Redwan Ahmed Adib <42737943+redwan247@users.noreply.github.com>
2019-04-23 23:54:08 +0600

    Add files via upload

---
+++ URI 1008 - Salary.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+hours = int(input())
+
+p_salary = float(input())
+
+
+
+f_salary = float(hours*p_salary)
+
+
+
+print("NUMBER = %d" %num)
+
+print("SALARY = U$ %0.2f" %f_salary)
---
+++ URI 1009 - Salary With Bonus.py
@@ -0,0 +1,13 @@
+name = input()
+
+salary = float(input())
+
+sales = float(input())
+
+
+
+f_salary = float(salary+(sales*0.15))
+
+
+
+print("TOTAL = R$ %0.2f" %f_salary)
